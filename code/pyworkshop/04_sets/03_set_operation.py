a70af1e0bcfc6f8e1dbcd540e736780f5b5142a0
Animesh Kumar Bhadra <animeshb@archerimagine.com>
2015-06-21 20:00:00 -0600

    Union Operation in set

---
+++ code/pyworkshop/04_sets/03_set_operation.py
@@ -0,0 +1,11 @@
+# Will learn 3 major operation.
+
+rainbow_color = {"Violet", "Indigo", "Blue", "Green", "Yellow", "Orange", "Red"}
+favourite_color = {"Red", "Green", "Blue", "Black"}
+
+# Union, |, .union()
+print(rainbow_color | favourite_color)
+print(rainbow_color.union(favourite_color))
+
+#
+
