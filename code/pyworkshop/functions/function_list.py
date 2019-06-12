557d973d1cb8b467f04d90a4026a06c9ae8606b8
Animesh Kumar Bhadra <animeshb@archerimagine.com>
2019-06-12 23:28:39 +0530

    Adding mutable objects as default arguments.

---
+++ code/pyworkshop/functions/function_list.py
@@ -0,0 +1,23 @@
+# Making a mutable object (list), as a default argument is a bad idea.
+# The list is created on the first call and then same is shared.
+
+
+def mutable_list(a, b=[]):
+    b.append(a)
+    print("B is ", b)
+
+
+mutable_list(3)  # B is  [3]
+mutable_list(4)  # B is  [3, 4]
+
+# We should do this.
+def new_list(a, b=None):
+    if b is None:
+        b = []
+    b.append(a)
+    print("B is ", b)
+
+
+new_list(2)  # B is  [2]
+new_list(4)  # B is  [4]
+new_list(4, [1, 2, 3])  # B is  [1, 2, 3, 4]
