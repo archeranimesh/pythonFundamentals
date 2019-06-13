1598b35a40f0ed7aa747668808ebd1195609f90d
Animesh Kumar Bhadra <animeshb@archerimagine.com>
2019-06-13 21:53:54 +0530

    created the list without the if condition.

--- code/pyworkshop/functions/function_list.py
+++ code/pyworkshop/functions/function_list.py
@@ -15,9 +15,21 @@ def new_list(a, b=None):
     if b is None:
         b = []
     b.append(a)
-    print("B is ", b)
+    print("B is  new_list()", b)
 
 
 new_list(2)  # B is  [2]
 new_list(4)  # B is  [4]
 new_list(4, [1, 2, 3])  # B is  [1, 2, 3, 4]
+
+# Pythonic Way
+def new_list_2(a, b=None):
+    b = b or []
+    b.append(a)
+    print("B is  new_list_2()", b)
+
+
+new_list_2(2)
+new_list_2(4)
+new_list_2(4, [1, 2, 3])
+
