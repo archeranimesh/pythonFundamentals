cd5a22b08c90f99ee15dafae3ab2b6d7c0dfcff1
Animesh Kumar Bhadra <animeshb@archerimagine.com>
2019-06-19 22:42:14 +0530

    started set, create empty set, convert list to set,

---
+++ code/pyworkshop/04_sets/01_sets.py
@@ -0,0 +1,23 @@
+# Create empty set, this is the only way.
+a = set()
+print(type(a))
+
+# {} creates a dict.
+print(type({}))
+
+# No duplicates in sets.
+names = {"aaaa", "bbbb", "cccc", "aaaa"}
+print(type(names))
+print(names)
+
+# Sets can be used to remove duplicates from list.
+names_list = ["aaaa", "bbbb", "cccc", "aaaa"]
+print(set(names_list))
+
+# Set stores only immutable type.
+# a = {"a", (1, 2, 3), [1, 2, 3]}  # TypeError: unhashable type: 'list'
+print(type(a))
+
+# to check we can use a function called hash()
+print(hash("aaaa"))
+# print(hash([]))  # TypeError: unhashable type: 'list'
