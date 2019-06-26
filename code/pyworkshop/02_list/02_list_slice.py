85af7317cdaf34870edaf38f3c0403d94ae156d4
Animesh Kumar Bhadra <animeshb@archerimagine.com>
2019-06-26 22:52:56 +0530

    added the list slice operation.

---
+++ code/pyworkshop/02_list/02_list_slice.py
@@ -0,0 +1,7 @@
+# We can get a part of list by using slice.
+my_list = ["h", "e", "l", "l", "o", "!"]
+
+print(my_list[0:3])  # Returns 0 - 2nd index
+print(my_list[:])  # clone full list
+print(my_list[-1])  # Special way to get last element
+
