34ba6a0a8b8df4c8ed4fd41eca814df088aab931
Animesh Kumar Bhadra <animeshb@archerimagine.com>
2019-07-04 22:34:49 +0530

    Looping over dict with keys, items, values and enumerate.

---
+++ code/pyworkshop/07_loops_if/02_dict_loops.py
@@ -0,0 +1,18 @@
+rainbow = {"Green": "G", "Red": "R", "Blue": "B"}
+
+# default iteration is on keys.
+for color in rainbow:
+    print(color)
+
+# iterate over both keys and value.
+for key, value in rainbow.items():
+    print(key, ":", value)
+
+# iterate over only value.
+for value in rainbow.values():
+    print(value)
+
+# ennumerate with index, and key value
+for i, (key, value) in enumerate(rainbow.items()):
+    print(i, key, value)
+
