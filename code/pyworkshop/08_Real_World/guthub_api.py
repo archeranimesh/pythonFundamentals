fecbf952dac6361c0ca648196860b90b0487ad7d
Animesh Kumar Bhadra <animeshb@archerimagine.com>
2019-07-13 22:57:36 +0530

    geting the result.

--- code/pyworkshop/08_Real_World/guthub_api.py
+++ code/pyworkshop/08_Real_World/guthub_api.py
@@ -6,8 +6,23 @@ PARAMS = {"q": "stars:>50000"}
 
 def repos_with_most_star():
     response = requests.get(GH_REPO_URL, params=PARAMS)
-    print("Response from GitHub: ", response.text)
+
+    response_items = response.json()["items"]
+
+    # print("Response from GitHub: ", response.text)
+    # print("Keys in response: ", response_json.keys())
+    # print("Total Count: ", response_json["total_count"])
+    # print("incomplete_results: ", response_json["incomplete_results"])
+
+    return response_items
 
 
 if __name__ == "__main__":
-    repos_with_most_star()
+    results = repos_with_most_star()
+    # print(results[0])
+    for result in results:
+        language = result["language"]
+        stars = result["stargazers_count"]
+        name = result["name"]
+
+        print(f"--> {name} is a {language} repo with {stars} stars.")
